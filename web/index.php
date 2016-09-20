2f119228396a029d63070f58e41a0018b93e084e
justinwaite <justin.waite@paqtrack.com>
2016-09-19 21:58:26 -0600

    Added a home page, about page, and assignments page.

--- web/index.php
+++ web/index.php
@@ -22,4 +22,14 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/about', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('about.twig');
+});
+
+$app->get('/assignments', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('assignments.twig');
+});
+
 $app->run();
